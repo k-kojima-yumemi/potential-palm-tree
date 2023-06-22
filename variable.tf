12a2a64adb83da3aab6e4ce13facbc8f4c4b5388
Kojima Kohei <125636684+k-kojima-yumemi@users.noreply.github.com>
2023-06-22 22:50:37 +0900

    Update description of variable

--- variable.tf
+++ variable.tf
@@ -1,11 +1,15 @@
 variable "aws_region" {
   type = string
+  description = "The region of AWS resources, like ap-northeast-1"
 }
 
 variable "gh_token" {
   sensitive = true
   type = string
-  description = "A token to access github, can be given via command line"
+  description = <<EOF
+A token to access github, can be given via command line
+`terraform plan -var "gh_token=$(gh auth token)"`
+EOF
 }
 
 variable "gh_owner" {
@@ -25,17 +29,21 @@ variable "gh_repo_env_name" {
 
 variable "gh_repo_env_secret_name" {
   type = string
+  description = "The name of secret in environment, where the IAM Role arn is saved"
 }
 
 variable "gh_repo_env_variable_name" {
   type = string
+  description = "The name of variable in environment, where the AWS Region is saved"
 }
 
 variable "aws_id_provider_arn" {
   type = string
   default = ""
+  description = "If give, the IAM Role is created with this arn. If empty, a new provider is created and used in role"
 }
 
 variable "aws_role_name" {
   type = string
+  description = "The name of IAM Role"
 }
